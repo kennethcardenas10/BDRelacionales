5e93d428aabf41873ef8647920e25a1d2355b41a
kennethcardenas10 <kenneth.cardenas24@cetis107.edu.mx>
2026-05-12 08:35:08 -0700

    localhost

---
+++ copa_del_mundo.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-05-2026 a las 17:34:27
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `copa_del_mundo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `confederacion`
+--
+
+CREATE TABLE `confederacion` (
+  `id_confederacion` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `siglas` varchar(10) NOT NULL,
+  `region` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `confederacion`
+--
+
+INSERT INTO `confederacion` (`id_confederacion`, `nombre`, `siglas`, `region`) VALUES
+(1, 'union de asociaciones europeas', 'UEFA', 'suiza');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+CREATE TABLE `equipo` (
+  `id_equipo` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `codigo_fifa` varchar(3) NOT NULL,
+  `entrenador` varchar(100) NOT NULL,
+  `id_confederacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadio`
+--
+
+CREATE TABLE `estadio` (
+  `id_estadio` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `ciudad` varchar(80) NOT NULL,
+  `pais` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugador`
+--
+
+CREATE TABLE `jugador` (
+  `id_jugador` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `posicion` varchar(30) NOT NULL,
+  `numero_dorsal` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `partido`
+--
+
+CREATE TABLE `partido` (
+  `id_partido` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `fase` varchar(30) NOT NULL,
+  `goles_local` tinyint(4) NOT NULL,
+  `goles_visitante` tinyint(4) NOT NULL,
+  `id_estadio` int(11) NOT NULL,
+  `id_local` int(11) NOT NULL,
+  `id_visitante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `confederacion`
+--
+ALTER TABLE `confederacion`
+  ADD PRIMARY KEY (`id_confederacion`);
+
+--
+-- Indices de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD PRIMARY KEY (`id_equipo`),
+  ADD KEY `id_confederacion` (`id_confederacion`);
+
+--
+-- Indices de la tabla `estadio`
+--
+ALTER TABLE `estadio`
+  ADD PRIMARY KEY (`id_estadio`);
+
+--
+-- Indices de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  ADD PRIMARY KEY (`id_jugador`);
+
+--
+-- Indices de la tabla `partido`
+--
+ALTER TABLE `partido`
+  ADD PRIMARY KEY (`id_partido`),
+  ADD KEY `id_local` (`id_local`),
+  ADD KEY `id_visitante` (`id_visitante`),
+  ADD KEY `partido_ibfk_1` (`id_estadio`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `confederacion`
+--
+ALTER TABLE `confederacion`
+  MODIFY `id_confederacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estadio`
+--
+ALTER TABLE `estadio`
+  MODIFY `id_estadio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  MODIFY `id_jugador` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `partido`
+--
+ALTER TABLE `partido`
+  MODIFY `id_partido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD CONSTRAINT `equipo_ibfk_1` FOREIGN KEY (`id_equipo`) REFERENCES `confederacion` (`id_confederacion`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  ADD CONSTRAINT `jugador_ibfk_1` FOREIGN KEY (`id_jugador`) REFERENCES `equipo` (`id_equipo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `partido`
+--
+ALTER TABLE `partido`
+  ADD CONSTRAINT `partido_ibfk_1` FOREIGN KEY (`id_visitante`) REFERENCES `partido` (`id_partido`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
